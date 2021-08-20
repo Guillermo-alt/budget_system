966c2764cf4a0647cea1614b153b1da4475cebdb
Guillermo-alt <guillermo76ov@hotmail.com>
2021-08-20 18:34:03 -0500

    fix modelValidationModel.js

--- app/model/modelUserValidationModel.js
+++ app/model/modelUserValidationModel.js
@@ -23,7 +23,7 @@ module.exports = {
             {
             id_user:Joi.string().max(2).min(1).required(),
             password: Joi.string().regex(/^[a-zA-Z0-9]{4,16}$/).min(4).required(),
-        }).with('userName', 'password')
+        }).with('id_user', 'password')
 
         
 };
