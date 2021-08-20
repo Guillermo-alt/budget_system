ff3fe569ace6fb4e6f89a2d0d7b5b1dfc284329c
Guillermo-alt <guillermo76ov@hotmail.com>
2021-08-20 18:50:08 -0500

    fix ge user data with autentication

--- app/view/view.users.js
+++ app/view/view.users.js
@@ -28,7 +28,7 @@ module.exports = async (app)=>{
 			let user = await controlUsers.retrieveUser(req.params.user);
 			res.status(200).json(user);
         } catch (error) {
-            res.status(500).json('error in the request views user')
+            res.status(500).json('error in the request views user or authentication fails')
         }
      });
 
